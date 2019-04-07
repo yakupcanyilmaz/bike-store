e424dfddabba5b9e0ab8fa45d566e97ea0d5bdad
DESKTOP-UO8E75E\Can <ycanyilmaz@hotmail.com>
2019-04-07 12:41:46 +0300

    Updates

---
+++ precache-manifest.35bda1eb5b347ec3a23fb275ec11c38f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f87f972d3bcec499b0aebcb3184f1f7",
+    "url": "/bike-store/static/media/logo.6f87f972.svg"
+  },
+  {
+    "revision": "c007dac041deedb5aeb8",
+    "url": "/bike-store/static/js/runtime~main.6d3fd4ae.js"
+  },
+  {
+    "revision": "3667ed2b6c007293d4ab",
+    "url": "/bike-store/static/js/main.1d7aa2d4.chunk.js"
+  },
+  {
+    "revision": "4849abbcc816b76c467f",
+    "url": "/bike-store/static/js/2.bc43c410.chunk.js"
+  },
+  {
+    "revision": "3667ed2b6c007293d4ab",
+    "url": "/bike-store/static/css/main.83c79a25.chunk.css"
+  },
+  {
+    "revision": "4849abbcc816b76c467f",
+    "url": "/bike-store/static/css/2.6bb05ef0.chunk.css"
+  },
+  {
+    "revision": "4e3983453d7b2387e5e8a61e4cd6ef9b",
+    "url": "/bike-store/index.html"
+  }
+];
