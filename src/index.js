279bb56b62f6d946d4766a3ea7e5290cb46169e1
DESKTOP-UO8E75E\Can <ycanyilmaz@hotmail.com>
2019-04-07 13:34:38 +0300

    router deleted

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import { ProductProvider } from "./context";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <ProductProvider>
-    <Router>
-      <App />
-    </Router>
+    {/* <Router> */}
+    <App />
+    {/* </Router> */}
   </ProductProvider>,
   document.getElementById("root")
 );
